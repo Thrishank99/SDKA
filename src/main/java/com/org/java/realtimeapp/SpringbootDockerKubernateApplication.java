aad93c3254368cba5fbceb921371f97ae05eb043
Thrishank99 <thrishank966@gmail.com>
2024-09-24 18:21:09 +0530

    fsdaWDSAZ

--- src/main/java/com/org/java/realtimeapp/SpringbootDockerKubernateApplication.java
+++ src/main/java/com/org/java/realtimeapp/SpringbootDockerKubernateApplication.java
@@ -2,13 +2,21 @@ package com.org.java.realtimeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootDockerKubernateApplication {
+public class SpringbootDockerKubernateApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDockerKubernateApplication.class, args);
 		System.out.println("hello");
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(SpringbootDockerKubernateApplication.class);
 	}
 
 }
